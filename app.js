a1bf15cafe9d060b62264d576b58adf9d63f02b8
dmitryhtsnk <116877726+dmitryhtsnk@users.noreply.github.com>
2024-02-08 17:35:33 +0200

    improvements
    added opportunity to share colors using the link

--- app.js
+++ app.js
@@ -1,68 +1,100 @@
-const cols = document.querySelectorAll('.col');
+const cols = document.querySelectorAll(".col");
 
-document.addEventListener('keydown', event => {
-    event.preventDefault();
-    if(event.code.toLocaleLowerCase() === 'space'){
-        setRandomColours();
+document.addEventListener("keydown", (event) => {
+  event.preventDefault();
+  if (event.code.toLocaleLowerCase() === "space") {
+    setRandomColours();
+  }
+});
+
+document.addEventListener("click", (event) => {
+  const type = event.target.dataset.type;
+
+  if (type === "lock") {
+    const node =
+      event.target.tagName.toLowerCase() === "i"
+        ? event.target
+        : event.target.children[0];
+
+    node.classList.toggle("fa-lock-open");
+    node.classList.toggle("fa-lock");
+  } else if (type === "copy") {
+    copyToClickboard(event.target.textContent);
+  }
+});
+
+function generateRandomColour() {
+  // RGB
+  // #FF0000
+  // #00FF00
+  // #0000FF
+
+  const hexCodes = "0123456789ABCDEF";
+
+  let color = "";
+  for (let i = 0; i < 6; i++) {
+    color += hexCodes[Math.floor(Math.random() * hexCodes.length)];
+  }
+  return "#" + color;
+}
+
+function copyToClickboard(text) {
+  return navigator.clipboard.writeText(text);
+}
+
+function setRandomColours(isInitial) {
+  const colors = isInitial ? getColorsFromHash() : [];
+  cols.forEach((col, index) => {
+    const isLocked = col.querySelector("i").classList.contains("fa-lock");
+    const text = col.querySelector("h2");
+    const button = col.querySelector("button");
+
+    if (isLocked) {
+      colors.push(text.textContent);
+      return;
     }
-})
 
-document.addEventListener('click', event => {
-    const type = event.target.dataset.type;
+    const color = isInitial 
+        ? colors[index] 
+            ? colors[index]
+            : chroma.random()
+        : chroma.random()
 
-    if(type === 'lock'){
-        const node = event.target.tagName.toLowerCase() === 'i'
-            ? event.target
-            : event.target.children[0]
-        
-        node.classList.toggle('fa-lock-open');
-        node.classList.toggle('fa-lock');
-    }else if(type === 'copy'){
-        copyToClickboard(event.target.textContent);
+    if (!isInitial) {
+      colors.push(color);
     }
-})
 
-function generateRandomColour(){
-    // RGB
-    // #FF0000
-    // #00FF00
-    // #0000FF
+    text.textContent = color;
+    col.style.background = generateRandomColour();
 
-    const hexCodes = '0123456789ABCDEF';
+    setTextColor(text, color);
+    setTextColor(button, color);
 
-    let color = '';
-    for(let i = 0; i < 6; i++){
-        color += hexCodes[Math.floor(Math.random() * hexCodes.length)];
-    }
-    return '#' + color
+    updateColorsHash(colors);
+  });
 }
 
-function copyToClickboard(text){
-    return navigator.clipboard.writeText(text);
+function setTextColor(text, color) {
+  const luminance = chroma(color).luminance();
+  text.style.color = luminance > "0.5" ? "black" : "white";
 }
 
-function setRandomColours() {
-    cols.forEach((col) => {
-        const isLocked = col.querySelector('i').classList.contains('fa-lock');
-        const text = col.querySelector('h2');
-        const button = col.querySelector('button');
-        const color = chroma.random();
-
-        if(isLocked){
-            return
-        }
-
-        text.textContent = color;
-        col.style.background = generateRandomColour();   
-
-        setTextColor(text, color);
-        setTextColor(button, color);
-    });
+function updateColorsHash(colors = []) {
+  document.location.hash = colors
+    .map((col) => {
+      return col.toString().substring(1);
+    })
+    .join("-");
 }
 
-function setTextColor(text, color){
-    const luminance = chroma(color).luminance();
-    text.style.color = luminance > '0.5' ? 'black' : 'white';
+function getColorsFromHash() {
+  if (document.location.hash.length > 1) {
+    return document.location.hash
+      .substring(1)
+      .split("-")
+      .map((color) => "#" + color);
+  }
+  return [];
 }
 
-setRandomColours();
+setRandomColours(true);
